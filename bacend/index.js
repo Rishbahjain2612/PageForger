8b5bdb1b58698a7a68645e20b8e9f106987f0af6
jassisamuran <samuran3132@gmail.com>
2024-02-09 15:44:36 +0530

    userModel for user add in index.js and make server for requests in index.js with express

--- bacend/index.js
+++ bacend/index.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 const db = require("./config/db");
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+app.listen(process.env.port, () => {
+  console.log(`server is running on ${process.env.port}`);
+});
