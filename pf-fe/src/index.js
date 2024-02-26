5302a920b13b00c9f82cd3bc7b520c7041133efb
Rishbahjain2612 <rishabhjain.01226@gmail.com>
2024-02-26 12:47:59 +0530

    auth rerendering

--- pf-fe/src/index.js
+++ pf-fe/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { AuthProvider } from "./utils/authProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
