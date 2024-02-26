80bcf18ca6000192f9bd2487a7e6bc9e935e494a
Rishbahjain2612 <rishabhjain.01226@gmail.com>
2024-02-26 19:17:58 +0530

    localStorage data object

--- pf-fe/src/pages/preview/index.js
+++ pf-fe/src/pages/preview/index.js
@@ -19,24 +19,26 @@ function Preview() {
     const navigate = useNavigate();
 
     const handleSave = () => {
-        // const jwtToken = localStorage.getItem("jwtToken");
-        // if (!jwtToken)
-        // {
-        //     navigate("/login");
-        //     return;
-        // }
+        const jwtToken = localStorage.getItem("Token");
+        if (!jwtToken)
+        {
+            navigate("/login");
+            return;
+        }
 
 
         const data = {};
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
-        if (value) {
-            data[key] = value;
+        for (let i = 0; i < localStorage.length; i++)
+        {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            if (value)
+            {
+                data[key] = value;
+            }
         }
+        console.log(data); // You can use the 'data' object as needed
     }
-    console.log(data); // You can use the 'data' object as needed
-}
 
     return (
         <>
